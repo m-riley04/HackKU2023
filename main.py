152fb69ca3f5bdea29de962db6285911066b83a0
Riley M <rileymeyerkorth@gmail.com>
2023-04-14 21:04:41 -0500

    Add most of the initial files and add working GUI

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from app import App
-
+from components.window import App
+from PyQt6.QtWidgets import QApplication
 
 def main():
-    app = App()
-    app.run()
+    app = QApplication([])
+    window = App()
+    app.exec()
 
 if __name__ == "__main__":
     main()
